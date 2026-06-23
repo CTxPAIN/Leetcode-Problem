85e94518a1f602ec8813f7f76cdac7f654bd7b40
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-06-23 10:21:43 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.02%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1;
+
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target)return mid;
+            //check arr is left or right sorted
+
+            //it is a left sorted if
+            if(nums[start]<=nums[mid]){
+                if(nums[mid]>=target && target>=nums[start]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                 }
+            }
+            // right sorted
+            else if(target>=nums[mid] && target<=nums[end]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+
+        }
+        return -1;
+    }
+};
