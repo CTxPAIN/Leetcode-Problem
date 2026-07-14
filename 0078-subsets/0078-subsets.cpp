1bc6ccfc639e2995c98b3316b6809d2af8ba92fc
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-07-14 20:54:58 +0530

    Time: 4 ms (7.95%), Space: 15.6 MB (11.37%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,7 +3,7 @@ public:
     void helper(vector<int>&nums,vector<int>ans,vector<vector<int>>&finalAns,int idx){
         if(idx==nums.size()){
             finalAns.push_back(ans);
-            return;
+            return;  
         }
         helper(nums,ans,finalAns,idx+1);
         ans.push_back(nums[idx]);
@@ -17,3 +17,24 @@ public:
         
     }
 };
+
+
+//class Solution {
+// public:
+//     void helper(vector<int>&nums,vector<int>ans,vector<vector<int>>&finalAns,int idx){
+//         if(idx==nums.size()){
+//             finalAns.push_back(ans);
+//             return;
+//         }
+//         helper(nums,ans,finalAns,idx+1);
+//         ans.push_back(nums[idx]);
+//         helper(nums,ans,finalAns,idx+1);
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<int>ans;
+//         vector<vector<int>>finalAns;
+//         helper(nums,ans,finalAns,0);
+//         return finalAns;
+        
+//     }
+// };
