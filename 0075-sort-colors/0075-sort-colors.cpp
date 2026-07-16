0f77d1192eb035149f0aeee8ab3fb00b9af6fc4b
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-07-16 14:43:36 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,21 +5,36 @@ public:
         int j = nums.size() - 1;
         int k = 0;
 
-        while (k <= j) {
-            if (nums[k] == 0) {
-                int temp = nums[i];
-                nums[i] = nums[k];
-                nums[k] = temp;
+        // while (k <= j) {
+        //     if (nums[k] == 0) {
+        //         int temp = nums[i];
+        //         nums[i] = nums[k];
+        //         nums[k] = temp;
+        //         i++;
+        //         k++;
+        //     }
+        //     else if (nums[k] == 2) {
+        //         int temp = nums[k];
+        //         nums[k] = nums[j];
+        //         nums[j] = temp;
+        //         j--;
+        //     }
+        //     else {
+        //         k++;
+        //     }
+        // }
+
+        while(k<=j){
+            if(nums[k]==0){
+                swap(nums[i],nums[k]);
                 i++;
                 k++;
             }
-            else if (nums[k] == 2) {
-                int temp = nums[k];
-                nums[k] = nums[j];
-                nums[j] = temp;
+            else if(nums[k]==2){
+                swap(nums[k],nums[j]);
                 j--;
             }
-            else {
+            else{
                 k++;
             }
         }
