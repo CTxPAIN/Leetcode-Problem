fc604638185426c7529e3073c604b82a57b020a1
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-07-17 21:47:08 +0530

    Time: 36 ms (78.1%), Space: 62.3 MB (97.01%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -27,3 +27,40 @@ public:
        return result;
     }
 };
+
+
+//START
+//                             helper(start=1)
+//                                ans = []
+//           -------------------------------------------------------
+//          |                           |                           |
+//        i=1                         i=2                         i=3
+//   ans.push(1)                 ans.push(2)                 ans.push(3)
+//          |                           |                           |
+//          V                           V                           V
+//   helper(start=2)             helper(start=3)             helper(start=4)
+//      ans = [1]                   ans = [2]                   ans = [3]
+//   --------------              --------------                    |
+//  |       |      |            |              |                  i=4
+// i=2     i=3    i=4          i=3            i=4            ans.push(4)
+// ans.    ans.   ans.         ans.           ans.                 |
+// push(2) push(3)push(4)      push(3)        push(4)              V
+//  |       |      |            |              |            helper(start=5)
+//  V       V      V            V              V               ans = [3,4]
+// [1,2]   [1,3]  [1,4]        [2,3]          [2,4]               SIZE=2
+// SIZE=2  SIZE=2 SIZE=2       SIZE=2         SIZE=2             SAVE RESULT
+// SAVE    SAVE   SAVE         SAVE           SAVE                  |
+//  |       |      |            |              |                  RETURN
+//  | - - - | - - -|            | - - - - - - -|                  .
+//  RETURN  .      .            RETURN         .                  .
+//  .       .      .            .              .                  .
+//  .       .      .            .              .             - - -ans.pop(4)
+//  .       .      .       - - -ans.pop(3) - - ans.pop(4)         .
+//  .       .      .            .              .                  .
+//  .  - - -ans.pop(3)          .              .                  .
+// ans.     .      .            .              .                  .
+// pop(2)   .      .            .              .                  .
+//  .       .      .            .              .             - - -ans.pop(3)
+//  .  - - - - - - ans.pop(4)   .              .
+//  .                           .              .
+//  - - - - - - - - - - - - - - ans.pop(2) - - -
