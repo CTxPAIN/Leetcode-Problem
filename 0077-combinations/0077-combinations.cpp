d523070bddafd7820d3f4d59f1a0567d23d335fe
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-07-17 21:19:55 +0530

    Time: 248 ms (6.97%), Space: 139.9 MB (6.55%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void helper(vector<vector<int>>&result,vector<int>ans,int start,int n,int k){
+        if(ans.size()==k){
+            result.push_back(ans);
+            return;
+        }
+       
+        
+       for(int i=start;i<=n;i++){
+        ans.push_back(i);
+        helper(result,ans,i+1,n,k);
+        ans.pop_back();
+       }
+      
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+    
+        vector<vector<int>>result;
+        vector<int>ans;
+        int idx=0;
+      
+    
+       helper(result,ans,1,n,k);
+       return result;
+    }
+};
