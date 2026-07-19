da5f571f5396d1c00e2395df5ce7919b2913dc1c
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-07-20 02:41:11 +0530

    Time: 39 ms (59.58%), Space: 105.8 MB (93.97%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        long long sum=0;
+        int ans=1;
+      for(int right=0;right<nums.size();right++){
+        sum+=nums[right];
+        long long target=nums[right];
+        while(target*(right-i+1)-sum>k){
+            sum-=nums[i];
+            i++;
+        }
+        ans=max(ans,right-i+1); 
+      }
+      return ans;
+    }
+};
