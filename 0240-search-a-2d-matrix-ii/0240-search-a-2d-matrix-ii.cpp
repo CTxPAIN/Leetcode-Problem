3cc4352814f073b8eb46948736e5689fe35a1f08
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-07-19 15:09:58 +0530

    Time: 43 ms (90.08%), Space: 18.8 MB (39.12%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,14 +1,40 @@
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+// class Solution {
+// public:
+
+
+
+//it give timme complexity of O(m*n)=O(n^2)
+
+
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int row=matrix.size();
+//         int col=matrix[0].size();
+//         for(int i=0;i<row;i++){
+//             for(int j=0;j<col;j++){
+//                 if(matrix[i][j]==target)return true;
+//             }
+
+//         }
+//         return false;
+//     }
+// };
+
+
+class Solution{
+    public:
+    
+    bool searchMatrix(vector<vector<int>>&matrix,int target){
         int row=matrix.size();
         int col=matrix[0].size();
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(matrix[i][j]==target)return true;
-            }
+        int i=0;
+        int j=matrix[0].size()-1;
 
+        while(i<matrix.size()&&j>=0){
+            if(matrix[i][j]==target)return true;
+            else if(matrix[i][j]>target)j--;
+            else i++;
         }
         return false;
     }
-};
+
+} ;
