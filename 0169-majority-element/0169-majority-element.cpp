79119e194fc0343f3786cc37d726ef679da66bb8
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-08-19 17:56:32 +0530

    Time: 6 ms (12.2%), Space: 28.3 MB (44.61%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    int majorityElement(vector<int>& arr) {
+      unordered_map<int,int>mp;
+      for(int nums:arr){
+        mp[nums]++;
+      }
+        
+      for(auto x:mp){
+        if(x.second>arr.size()/2){
+            return x.first;
+        }
+      }
+      return arr[0];
+    }
+
+
+        
+};
