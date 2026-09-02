dbc2e2b65fa7cef83037ea0117f925cb6a1813ed
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-09-02 18:02:36 +0530

    Time: 3 ms (21.5%), Space: 21.8 MB (71.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -10,3 +10,27 @@ public:
         return nums.size();
     }
     };
+
+
+    // using map
+    //class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+
+//         unordered_map<int, int> mp;
+
+//         // Store all numbers
+//         for (int x : nums) {
+//             mp[x]++;
+//         }
+
+//         // Check 0 to n
+//         for (int i = 0; i <= nums.size(); i++) {
+//             if (mp.find(i) == mp.end()) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
