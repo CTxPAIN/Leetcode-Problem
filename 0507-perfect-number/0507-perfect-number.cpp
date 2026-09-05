d080929cba0a55d95466a53dcd4e60c16c399728
Thakur Prince singh <154059731+CTxPAIN@users.noreply.github.com>
2026-09-05 18:22:13 +0530

    Time: 2115 ms (5.8%), Space: 7.7 MB (54.93%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int sum=0;
+        if(num<=1)return false;
         for(int i=1;i<num;i++){
             if(num%i==0){
                 sum+=i;
